9d69939f47f49a427ed3ef00102e710786f2f867
Vaskar Chandra Das <vcdas123@gmail.com>
2023-06-25 11:55:12 +0530

    up

--- Backend/app.js
+++ Backend/app.js
@@ -4,6 +4,7 @@ const morgan = require('morgan');
 const cookieParser = require('cookie-parser');
 const productRouter = require('./routes/productRoutes');
 const userRouter = require('./routes/userRoutes');
+const reviewRouter = require('./routes/reviewRoutes');
 const globalErrorController = require('./controllers/globalErrorController');
 const AppError = require('./utils/appError');
 
@@ -20,6 +21,7 @@ if (process.env.NODE_ENV === 'development') app.use(morgan('dev'));
 //~ Original Url =  http://localhost:8000/api/v1/
 app.use('/api/v1/products', productRouter);
 app.use('/api/v1/users', userRouter);
+app.use('/api/v1/reviews', reviewRouter);
 
 //! Undefined Routes
 app.use('*', (req, res, next) => next(new AppError('Requested resource not found ˙◠˙ ', 404)));
